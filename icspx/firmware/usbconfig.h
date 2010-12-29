e28f11b2c1e87749765a3dce2e836d181679ff45
Marcos Piccinini <x@nofxx.com>
2010-12-29 09:05:13 -0200

    mods

--- icspx/firmware/usbconfig.h
+++ icspx/firmware/usbconfig.h
@@ -159,8 +159,8 @@ the newest features and options.
  * obdev's free shared VID/PID pair. See the file USBID-License.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME   'U', 'S', 'B', 'a', 's', 'p','A', 'V', 'B'
-#define USB_CFG_DEVICE_NAME_LEN 9
+#define USB_CFG_DEVICE_NAME   'U', 'S', 'B', 'a', 's', 'p'
+#define USB_CFG_DEVICE_NAME_LEN 6
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USBID-License.txt before you assign a name.
  */
